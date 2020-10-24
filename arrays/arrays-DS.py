ca3fb3f19c655e23599b51e1cf027c51b06966a1
Zachari Ramos <zachariramos@Zacharis-MacBook-Air.local>
2020-10-24 14:01:17 -0500

    did hackerrank array problem

---
+++ arrays/arrays-DS.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the reverseArray function below.
+def reverseArray(a):
+    rArray = []
+    for x in range(1, len(a)):
+        rArray.append(a[-x])
+    rArray.append(a[0])
+
+    return rArray
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = reverseArray(arr)
+
+    fptr.write(' '.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
+
