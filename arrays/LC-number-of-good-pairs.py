17f0e8b094cfa651f90688dd5b6dc2cc5519da42
Zachari Ramos <zachariramos@gmail.com>
2020-10-27 12:42:09 -0500

    another leetcode problem

---
+++ arrays/LC-number-of-good-pairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+#If we have k occerance of n, where n is number in nums array, then we can have k*(k-1)/2 good pairs.
+#Keeping this in mind, and using set we can get following code.
+      count = 0 
+      for x in set(nums):
+        y = nums.count(x)
+        count = count + int((y*(y-1)) / 2)
+      
+      return count
