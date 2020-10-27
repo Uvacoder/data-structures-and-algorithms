4fd8c1e77a17ba6a079cefe03906954322152567
Zachari Ramos <zachariramos@gmail.com>
2020-10-27 12:46:22 -0500

    edited comments

--- arrays/LC-number-of-good-pairs.py
+++ arrays/LC-number-of-good-pairs.py
@@ -2,6 +2,7 @@ class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
 #If we have k occerance of n, where n is number in nums array, then we can have k*(k-1)/2 good pairs.
 #Keeping this in mind, and using set we can get following code.
+#Using nC2 Logic
       count = 0 
       for x in set(nums):
         y = nums.count(x)
