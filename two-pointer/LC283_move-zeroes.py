6739901bf70bba635672cacbd5168174002c2aa1
Zachari Ramos <zachariramos@gmail.com>
2021-02-03 15:17:14 -0600

    lc283 problem

---
+++ two-pointer/LC283_move-zeroes.py
@@ -0,0 +1,26 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        You must do this in-place without making a copy of the array.
+        """
+        #two pointer approach
+        
+        # Count the zeros
+        numOfZeros = 0
+        for x in nums:
+            if x == 0:
+                numOfZeros += 1
+                
+        # Same method as in LC27
+        i = 0
+        for x in nums:
+            if x != 0:
+                nums[i] = x
+                i += 1
+        
+        if numOfZeros == 1:
+            nums[-numOfZeros] = 0
+        elif numOfZeros != 0: 
+            for w in range(1, numOfZeros + 1):
+                nums[-w] = 0
