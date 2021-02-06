70e773c304551cdff894b0bdc7ba08195bfc99b7
Zachari Ramos <zachariramos@gmail.com>
2021-02-05 19:06:04 -0600

    finished LC228

--- two-pointer/LC228_summary-ranges.py
+++ two-pointer/LC228_summary-ranges.py
@@ -6,6 +6,10 @@ class Solution:
         b = 1    # this will be our fast pointer
         lenNums = len(nums) 
         
+        if lenNums == 0:
+            return []
+        
+        
         summary_range = [ str(nums[0]) ]    # adds the beginning of the range
         while a < lenNums and b < lenNums:
             if nums[b] - nums[a] == 1:  # if they subtract to 1 that means they are in order
@@ -18,7 +22,9 @@ class Solution:
                 a = b
                 summary_range.append( str(nums[a]) )     # adds teh beginning of the range
                 b += 1
-                
+        
+        
+        if summary_range[-1] != str(nums[a]):
+            summary_range[-1] = summary_range[-1] + "->" + str(nums[a])
             
-                
         return summary_range
