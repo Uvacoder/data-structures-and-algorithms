f0a97e1ce670f7001c993e991435d08bfe5517e7
Zachari Ramos <zachariramos@gmail.com>
2021-02-16 18:35:45 -0600

    lc53

---
+++ dynamic-programming/LC53_maximum-subarray.py
@@ -0,0 +1,15 @@
+# this problem is also found in the 'array' directory
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        # dynamic programming approach
+        largest_sum = nums[0] 
+        current = nums[0]
+        
+        for x in nums[1:]:  # start from 1st index 
+            current = max(x, current + x)  # finding the biggest sum
+            
+            if current > largest_sum:
+                largest_sum = current
+            
+        return largest_sum 
